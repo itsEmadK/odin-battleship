3af07ad520013cb5ac23e3d5d64e30ce2c757a64
Emad <67466634+itsEmadK@users.noreply.github.com>
2025-03-04 20:11:14 +0330

    Add placeShip method

--- src/game-board.js
+++ src/game-board.js
@@ -13,6 +13,25 @@ class GameBoard {
             }
         }
     }
+
+    placeShipHorizontally(ship, x, y) {
+        if (x >= this.gridSize || y >= this.gridSize) {
+            throw new Error('coordinate should be inside the board');
+        }
+        if (x + ship.length >= this.gridSize || y >= this.gridSize) {
+            throw new Error('ship is too large for the board');
+        }
+        for (let i = 0; i < ship.length; i++) {
+            if (this.board[y][x + i].ship !== null) {
+                return false;
+            }
+        }
+
+        for (let i = 0; i < ship.length; i++) {
+            this.board[y][x + i].ship = ship;
+        }
+        return true;
+    }
 }
 
 export { GameBoard };
--- src/game-board.test.js
+++ src/game-board.test.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import { GameBoard } from './game-board.js';
+import { Ship } from './ship.js';
 
 describe('GameBoard constructor', () => {
     test('exists', () => {
@@ -41,5 +42,100 @@ describe('GameBoard constructor', () => {
                 }
             }
         });
+    });
+});
+
+describe('placeShipHorizontally Method', () => {
+    test('exists', () => {
+        const gb = new GameBoard();
+        expect(gb.placeShipHorizontally).toBeDefined();
+    });
+    test('places a ship of length 1 on x:3, y:5', () => {
+        const gb = new GameBoard();
+        const ship = new Ship(1);
+        const x = 3;
+        const y = 5;
+        gb.placeShipHorizontally(ship, x, y);
+        expect(gb.board[y][x].ship).toBe(ship);
+    });
+    test('places a ship of length 3 on x:0, y:2', () => {
+        const gb = new GameBoard();
+        const ship = new Ship(3);
+        const x = 0;
+        const y = 2;
+        gb.placeShipHorizontally(ship, x, y);
+        expect(gb.board[y][x].ship).toBe(ship);
+        expect(gb.board[y][x + 1].ship).toBe(ship);
+        expect(gb.board[y][x + 2].ship).toBe(ship);
+    });
+    test('places a ship of length 5 on x:1, y:0', () => {
+        const gb = new GameBoard();
+        const ship = new Ship(5);
+        const x = 1;
+        const y = 0;
+        gb.placeShipHorizontally(ship, x, y);
+        expect(gb.board[y][x].ship).toBe(ship);
+        expect(gb.board[y][x + 1].ship).toBe(ship);
+        expect(gb.board[y][x + 2].ship).toBe(ship);
+        expect(gb.board[y][x + 3].ship).toBe(ship);
+        expect(gb.board[y][x + 4].ship).toBe(ship);
+    });
+    test('returns true if the ship was placed on the board', () => {
+        const gb = new GameBoard();
+        const ship = new Ship(5);
+        const x = 1;
+        const y = 0;
+        expect(gb.placeShipHorizontally(ship, x, y)).toBeTruthy();
+    });
+    test('throws error if coordinate is outside the board', () => {
+        const gb = new GameBoard();
+        const ship = new Ship(2);
+        const x = 10;
+        const y = 10;
+        expect(() => gb.placeShipHorizontally(ship, x, y)).toThrow(
+            'coordinate should be inside the board',
+        );
+    });
+    test('throws error if the ship is too long for the board', () => {
+        const gb = new GameBoard(5);
+        const ship = new Ship(6);
+        const x = 0;
+        const y = 0;
+        expect(() => gb.placeShipHorizontally(ship, x, y)).toThrow(
+            'ship is too large for the board',
+        );
+    });
+    test('does not place ship if cell is occupied', () => {
+        const gb = new GameBoard();
+        const ship1 = new Ship(4);
+        const x = 0;
+        const y = 0;
+        gb.placeShipHorizontally(ship1, x, y);
+        const ship2 = new Ship(3);
+        gb.placeShipHorizontally(ship2, x, y);
+        expect(gb.board[y][x].ship).toBe(ship1);
+        expect(gb.board[y][x + 1].ship).toBe(ship1);
+        expect(gb.board[y][x + 2].ship).toBe(ship1);
+    });
+    test('does not place ship if any of ship cells is occupied by another ship', () => {
+        const gb = new GameBoard();
+        const ship1 = new Ship(4);
+        const x = 4;
+        const y = 0;
+        gb.placeShipHorizontally(ship1, x, y);
+        const ship2 = new Ship(3);
+        gb.placeShipHorizontally(ship2, 2, 0);
+        expect(gb.board[0][2].ship).toBe(null);
+        expect(gb.board[0][3].ship).toBe(null);
+        expect(gb.board[0][4].ship).toBe(ship1);
+    });
+    test('returns false if coordinate is inside the board but any of the cells is occupied', () => {
+        const gb = new GameBoard();
+        const ship1 = new Ship(4);
+        const x = 4;
+        const y = 0;
+        gb.placeShipHorizontally(ship1, x, y);
+        const ship2 = new Ship(3);
+        expect(gb.placeShipHorizontally(ship2, 2, 0)).toBe(false);
     });
 });
