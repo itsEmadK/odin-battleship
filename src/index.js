ef7dc8f6bedb2ddee1aaf7ce88a0ebc5095d569e
Emad <67466634+itsEmadK@users.noreply.github.com>
2025-03-05 22:24:44 +0330

    Handle player wins

--- src/index.js
+++ src/index.js
@@ -17,18 +17,18 @@ displayController.renderPlayer2Board(player2.gameBoard.board);
 const player1BoardDiv = document.querySelector('.board.player1');
 const player2BoardDiv = document.querySelector('.board.player2');
 
+const dialog = document.querySelector('dialog.game-over');
+const playAgainButton = dialog.querySelector('.play-again');
+playAgainButton.addEventListener('click', () => {
+    dialog.close();
+    player1.gameBoard.resetBoard();
+    player2.gameBoard.resetBoard();
+    player1.gameBoard.populate();
+    player2.gameBoard.populate();
+    displayController.renderPlayer1Board(player1.gameBoard.board);
+    displayController.renderPlayer2Board(player2.gameBoard.board);
+});
 player1BoardDiv.classList.add('revealed');
-// player1BoardDiv.addEventListener('click', (e) => {
-//     if ([...e.target.classList].includes('cell')) {
-//         if (turn === 2) {
-//             const x = +e.target.dataset.x;
-//             const y = +e.target.dataset.y;
-//             player1.gameBoard.receiveAttack(x, y);
-//             displayController.renderPlayer1Board(player1.gameBoard.board);
-//             turn = 1;
-//         }
-//     }
-// });
 
 player2BoardDiv.addEventListener('click', (e) => {
     if ([...e.target.classList].includes('cell')) {
@@ -37,21 +37,34 @@ player2BoardDiv.addEventListener('click', (e) => {
             const y = +e.target.dataset.y;
             if (player2.gameBoard.receiveAttack(x, y)) {
                 displayController.renderPlayer2Board(player2.gameBoard.board);
-                turn = 2;
-                if (player2 instanceof AIPlayer) {
-                    const delay = new Promise((resolve, reject) => {
-                        setTimeout(resolve, 500);
-                    });
+                if (player2.gameBoard.areAllShipsSunk()) {
+                    player1.score++;
+                    dialog.showModal();
+                } else {
+                    turn = 2;
+                    if (player2 instanceof AIPlayer) {
+                        const delay = new Promise((resolve) => {
+                            setTimeout(resolve, 500);
+                        });
 
-                    delay.then(() => {
-                        const { x: aiAttackX, y: aiAttackY } =
-                            player2.getNextAttack();
-                        player1.gameBoard.receiveAttack(aiAttackX, aiAttackY);
-                        displayController.renderPlayer1Board(
-                            player1.gameBoard.board,
-                        );
-                        turn = 1;
-                    });
+                        delay.then(() => {
+                            const { x: aiAttackX, y: aiAttackY } =
+                                player2.getNextAttack();
+                            player1.gameBoard.receiveAttack(
+                                aiAttackX,
+                                aiAttackY,
+                            );
+                            displayController.renderPlayer1Board(
+                                player1.gameBoard.board,
+                            );
+                            if (player1.gameBoard.areAllShipsSunk()) {
+                                player2.score++;
+                                dialog.showModal();
+                            } else {
+                                turn = 1;
+                            }
+                        });
+                    }
                 }
             }
         }
