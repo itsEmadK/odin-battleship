56acf755662fcdae7c825cb9bfa1645da8e8ce6e
Emad <67466634+itsEmadK@users.noreply.github.com>
2025-03-05 14:03:08 +0330

    Add event listener for player2's board clicks

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import { Player } from './player.js';
+import { AIPlayer } from './ai-player.js';
+import { displayController } from './displayController.js';
+import './styles.css';
+
+let turn = 1;
+
+const player1 = new Player('Player1');
+player1.gameBoard.populate();
+
+const player2 = new AIPlayer();
+player2.gameBoard.populate();
+
+displayController.renderPlayer1Board(player1.gameBoard.board);
+displayController.renderPlayer2Board(player2.gameBoard.board);
+
+// const player1BoardDiv = document.querySelector('.board.player1');
+const player2BoardDiv = document.querySelector('.board.player2');
+
+// player1BoardDiv.addEventListener('click', (e) => {
+//     if ([...e.target.classList].includes('cell')) {
+//         if (turn === 2) {
+//             const x = +e.target.dataset.x;
+//             const y = +e.target.dataset.y;
+//             player1.gameBoard.receiveAttack(x, y);
+//             displayController.renderPlayer1Board(player1.gameBoard.board);
+//             turn = 1;
+//         }
+//     }
+// });
+
+player2BoardDiv.addEventListener('click', (e) => {
+    if ([...e.target.classList].includes('cell')) {
+        if (turn === 1) {
+            const x = +e.target.dataset.x;
+            const y = +e.target.dataset.y;
+            player2.gameBoard.receiveAttack(x, y);
+            displayController.renderPlayer2Board(player2.gameBoard.board);
+            turn = 2;
+        }
+    }
+});
