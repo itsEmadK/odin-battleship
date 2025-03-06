655439353e81eb18643cc73ade8a9220392252c2
Emad <67466634+itsEmadK@users.noreply.github.com>
2025-03-06 12:28:19 +0330

    Handle using onGameOverDialogLoaded

--- src/index.js
+++ src/index.js
@@ -11,42 +11,56 @@ player1.gameBoard.populate();
 const player2 = new AIPlayer();
 player2.gameBoard.populate();
 
-const handlePlayer1sWin = () => {
-    player1.score++;
+function onGameOverDialogLoaded() {
     const dialog = document.querySelector('dialog.game-over');
-    const h3 = dialog.querySelector('h3');
-    h3.innerText = 'You won!';
-    dialog.classList.remove('loss');
-    dialog.classList.add('win');
-    dialog.showModal();
-};
-const handlePlayer2sWin = () => {
-    player2.score++;
-    const dialog = document.querySelector('dialog.game-over');
-    const h3 = dialog.querySelector('h3');
-    h3.innerText = 'You lost :(';
-    dialog.classList.remove('win');
-    dialog.classList.add('loss');
-    dialog.showModal();
-};
-const playAITurn = () => {
-    const delay = new Promise((resolve) => {
-        setTimeout(resolve, AIResponseLatencyMS);
+    const playAgainButton = dialog.querySelector('button');
+    playAgainButton.addEventListener('click', () => {
+        dialog.close();
+        player1.gameBoard.resetBoard();
+        player2.gameBoard.resetBoard();
+        player1.gameBoard.populate();
+        player2.gameBoard.populate();
+        displayController.loadMainScreen(player1, player2, onMainScreenLoaded);
+        const player1BoardDiv = document.querySelector('div.board.player1');
+        player1BoardDiv.classList.add('revealed');
     });
+}
+function onMainScreenLoaded() {
+    const handlePlayer1sWin = () => {
+        player1.score++;
+        displayController.loadGameOverDialog('You won!', () => {
+            const dialog = document.querySelector('dialog.game-over');
+            dialog.classList.remove('loss');
+            dialog.classList.add('win');
+            onGameOverDialogLoaded();
+        });
+    };
+    const handlePlayer2sWin = () => {
+        player2.score++;
+        const dialog = document.querySelector('dialog.game-over');
+        const h3 = dialog.querySelector('h3');
+        h3.innerText = 'You lost :(';
+        dialog.classList.remove('win');
+        dialog.classList.add('loss');
+        dialog.showModal();
+    };
+    const playAITurn = () => {
+        const delay = new Promise((resolve) => {
+            setTimeout(resolve, AIResponseLatencyMS);
+        });
 
-    delay.then(() => {
-        const { x: aiAttackX, y: aiAttackY } = player2.getNextAttack();
-        player1.gameBoard.receiveAttack(aiAttackX, aiAttackY);
-        displayController.renderPlayer1Board(player1.gameBoard.board);
-        if (player1.gameBoard.areAllShipsSunk()) {
-            handlePlayer2sWin();
-        } else {
-            turn = 1;
-        }
-    });
-};
+        delay.then(() => {
+            const { x: aiAttackX, y: aiAttackY } = player2.getNextAttack();
+            player1.gameBoard.receiveAttack(aiAttackX, aiAttackY);
+            displayController.renderPlayer1Board(player1.gameBoard.board);
+            if (player1.gameBoard.areAllShipsSunk()) {
+                handlePlayer2sWin();
+            } else {
+                turn = 1;
+            }
+        });
+    };
 
-displayController.loadMainScreen(player1, player2, () => {
     const player1BoardDiv = document.querySelector('.board.player1');
     player1BoardDiv.classList.add('revealed');
 
@@ -72,15 +86,6 @@ displayController.loadMainScreen(player1, player2, () => {
             }
         }
     });
-});
+}
 
-// playAgainButton.addEventListener('click', () => {
-//     dialog.close();
-//     player1.gameBoard.resetBoard();
-//     player2.gameBoard.resetBoard();
-//     player1.gameBoard.populate();
-//     player2.gameBoard.populate();
-//     displayController.renderPlayer1Board(player1.gameBoard.board);
-//     displayController.renderPlayer2Board(player2.gameBoard.board);
-// });
-// player1BoardDiv.classList.add('revealed');
+displayController.loadMainScreen(player1, player2, onMainScreenLoaded);
